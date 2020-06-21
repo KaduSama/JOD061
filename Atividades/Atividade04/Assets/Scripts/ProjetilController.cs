43ad7d5dfcd7042bbe2f287a2ba9c710f2f99183
Carlos Eduardo da Silva Filho <36735720+KaduSama@users.noreply.github.com>
2020-06-20 21:49:23 -0300

    atividade04
    Criando o projeto Atividade04

---
+++ Atividades/Atividade04/Assets/Scripts/ProjetilController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class ProjetilController : NetworkBehaviour
+{
+    public float moveSpeed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, Time.deltaTime * 20f);
+    }
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        NetworkServer.Destroy(gameObject);
+        NetworkServer.Destroy(other.gameObject);
+    }
+}
